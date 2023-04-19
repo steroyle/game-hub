dfa4c7681e6e59f9e8e918f4a7e1c11a0335a7d8
steroyle <steroyle@gmail.com>
2023-04-19 23:06:31 +0100

    Added platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,19 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface GameProps {
   game: Game;
 }
 
 const GameCard = ({ game }: GameProps) => {
+  {console.log(game.parent_platforms)}
   return (
     <Card borderRadius={10} overflow='hidden'>
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(({ platform }) => platform)} />
       </CardBody>
     </Card>
   )
