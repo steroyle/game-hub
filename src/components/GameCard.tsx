2750266c17ba78bcd52ec0f1208e3ebd75679aa3
steroyle <steroyle@gmail.com>
2023-04-20 09:33:51 +0100

    Added critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CricticScore from "./CriticScore";
 
 interface GameProps {
   game: Game;
@@ -13,7 +14,10 @@ const GameCard = ({ game }: GameProps) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(({ platform }) => platform)} />
+        <HStack justifyContent='space-between'>
+          <PlatformIconList platforms={game.parent_platforms.map(({ platform }) => platform)} />
+          <CricticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   )
