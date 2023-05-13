5bd01ecc89611a9aea189da05689206559f02501
steroyle <steroyle@gmail.com>
2023-05-13 21:41:50 +0100

    Updated platform hook to use React Query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
         {selectedPlatform?.name || 'Platform'}
       </MenuButton>
       <MenuList>
-        {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
+        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
       </MenuList>
     </Menu>
   )
