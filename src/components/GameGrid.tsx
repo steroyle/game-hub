2afb0966f4d939595986df4c091388aacd7ccb9e
steroyle <steroyle@gmail.com>
2023-05-13 22:12:01 +0100

    Removed duplicate interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,7 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import useGames, {  } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import { GameQuery } from "../App";
 
 interface Props {
