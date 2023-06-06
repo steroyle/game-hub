8d72269678dd1503c850ab7ffaffe2880fd68df9
steroyle <steroyle@gmail.com>
2023-06-06 22:01:20 +0100

    Added game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import useGame from '../hooks/useGame';
+import GameAttributes from '../components/GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading mb={4}>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   )
 }
